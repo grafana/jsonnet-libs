1f2c4a608d377f6a67bcd9b8dec7fb6e72649929
David Kaltschmidt <david.kaltschmidt@gmail.com>
2017-11-22 11:46:24 +0100

    Print prometheus API stats
    API stats for prom queryies were introduced in
    https://github.com/prometheus/prometheus/pull/2408
    
    This PR adds a stats section in the top right to show those.

--- promeditor/src/App.js
+++ promeditor/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import './App.css';
 import QueryField from './QueryField';
+import Stats from './Stats';
 
 class App extends Component {
   state = {
     latency: null,
     result: null,
+    stats: null,
   }
 
   handleRequestError({ error, url }) {
@@ -28,42 +30,48 @@ class App extends Component {
     const { query } = this;
     if (!query) return;
 
-    this.setState({ latency: 0, result: null });
+    this.setState({ latency: 0, result: null, stats: {} });
 
     try {
       const jetzt = Date.now();
       const end = jetzt / 1000;
       const encodedQuery = encodeURIComponent(query);
-      const url = `/api/v1/query?query=${encodedQuery}&end=${end}&step=&_=${jetzt}`;
+      const url = `/api/v1/query?query=${encodedQuery}&end=${end}&stats=1&_source=promeditor&_=${jetzt}`;
       const res = await fetch(url);
       const result = await res.json();
       const latency = Date.now() - jetzt;
-      this.setState({ latency, result });
+      const { data = {} } = result;
+      const { stats = {} } = data;
+      this.setState({ latency, result, stats });
     } catch (error) {
       this.setState({ result: error });
     }
   }
 
   render() {
-    const { latency, result } = this.state;
+    const { latency, result, stats } = this.state;
     return (
       <div className="app pr4 pl4 pt3 pb4">
         <header>
-          <div className="h1 mb2">PromQL Editor</div>
-          <div className="app__input">
-            <QueryField
-              onPressEnter={this.handleSubmit}
-              onQueryChange={this.handleQueryChange}
-              onRequestError={this.handleRequestError}
-            />
+          <div className="flex-auto">
+            <div className="h1 mb2 mt3">PromQL Editor</div>
+            <div className="app__input">
+              <QueryField
+                onPressEnter={this.handleSubmit}
+                onQueryChange={this.handleQueryChange}
+                onRequestError={this.handleRequestError}
+              />
+            </div>
+            <button className="ml2 p1 button" onClick={this.handleSubmit}>
+              Run
+            </button>
           </div>
-          <button className="ml2 p1 button" onClick={this.handleSubmit}>
-            Run
-          </button>
-          {latency ? <span className="ml2 h6">Took {latency}ms</span> : null}
+          <div className="app__stats pr1">
+            {latency ? <Stats stats={stats} latency={latency} /> : null}
+          </div>
         </header>
         <main className="mt2 p1 rounded">
-          <pre>
+          <pre className="h5">
             {result ? JSON.stringify(result, null, '  ') : 'Query result' }
           </pre>
         </main>
---
+++ promeditor/src/Stats.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './Stats.css';
+
+export default ({ latency, stats }) => {
+  const { timings = {} } = stats || {};
+  // All timings are secs
+  const total = latency / 1000;
+  const rows = {
+    ...timings,
+    request: total,
+  };
+  return (
+    <div className="stats">
+      {Object.keys(rows)
+        .sort((a, b) => rows[a] - rows[b])
+        .map(t => {
+          // Treat everything as millies
+          const value = rows[t];
+          const ms = (value * 1000).toFixed(3);
+          const width = `${value / total * 100}%`;
+          return (
+            <div className="stats__row">
+              <div className="stats__values">
+                <span className="stats__label h6">{t}: </span>
+                <span className="stats__value h6 right-align">{ms}ms</span>
+              </div>
+              <div className="stats__line" style={{ width }} />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
