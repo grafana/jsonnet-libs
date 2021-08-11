grafana/jsonnet-libs

commit a20c679492aae363414dce23bc63da09cf8a7287

Make Secret name for GEM admin token configurable (#600)
* Make Secret name for GEM admin token configurable

* chore: Update generated documentation

Co-authored-by: Jack Baldry <jack.baldry@grafana.com>

enterprise-metrics/main.libsonnet
@@ -169,6 +169,21 @@ local removeNamespaceReferences(args) = std.map(function(arg) std.strReplace(arg
       type=d.T.string,
     ),
     licenseSecretName: 'gem-license',
+    '#adminTokenSecretName':: d.val(
+      default=self.adminTokenSecretName,
+      help=|||
+        When generating an admin token using the `tokengen` target, the result is written to the Kubernetes
+        Secret `adminTokenSecretName`.
+        There are two versions of the token in the Secret: `token` is the raw token obtained from the `tokengen`,
+        and `grafana-token` is a base64 encoded version that can be used directly when provisioning Grafana
+        via configuration file.
+        To retrieve these tokens from Kubernetes using `kubectl`:
+        $ kubectl get secret gem-admin-token -o jsonpath="{.data.token}" | base64 --decode ; echo
+        $ kubectl get secret gem-admin-token -o jsonpath="{.data.grafana-token}" | base64 --decode ; echo
+      |||,
+      type=d.T.string,
+    ),
+    adminTokenSecretName: 'gem-admin-token',
   },
   '#_images':: d.obj('`_images` contains fields for container images.'),
   _images:: {
@@ -573,9 +588,13 @@ local removeNamespaceReferences(args) = std.map(function(arg) std.strReplace(arg
     createSecretContainer::
       container.new('create-secret', this._images.kubectl)
       + container.withCommand([
-        '/bin/bash',
-        '-euc',
-        'kubectl create secret generic gem-admin-token --from-file=token=/shared/admin-token --from-literal=grafana-token="$(base64 <(echo :$(cat /shared/admin-token)))"',
+        'kubectl',
+        'create',
+        'secret',
+        'generic',
+        this._config.adminTokenSecretName,
+        '--from-file=token=/shared/admin-token',
+        '--from-literal=grafana-token="$(base64 <(echo :$(cat /shared/admin-token)))"',
       ])
       + container.withVolumeMounts([{ mountPath: '/shared', name: 'shared' }])
       // Need to run as root because the GEM container does.

commit 2c593dcc5b9223572b2bac465d5d37cb17d0372c

[metrics-enterprise] Fix Kubernetes Secret generation in GEM provisioner
This partially reverts commit a20c679492aae363414dce23bc63da09cf8a7287 which
introduced a bug that wrote `"$(base64 <(echo :$(cat /shared/admin-token)))"`
as Kubernetes secret value because the shell subcommand was not resolved.

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

enterprise-metrics/main.libsonnet
@@ -619,13 +619,9 @@ local removeNamespaceReferences(args) = std.map(function(arg) std.strReplace(arg
     createSecretContainer::
       container.new('create-secret', this._images.kubectl)
       + container.withCommand([
-        'kubectl',
-        'create',
-        'secret',
-        'generic',
-        this._config.adminTokenSecretName,
-        '--from-file=token=/shared/admin-token',
-        '--from-literal=grafana-token="$(base64 <(echo :$(cat /shared/admin-token)))"',
+        '/bin/bash',
+        '-euc',
+        'kubectl create secret generic %s --from-file=token=/shared/admin-token --from-literal=grafana-token="$(base64 <(echo :$(cat /shared/admin-token)))"' % this._config.adminTokenSecretName,
       ])
       + container.withVolumeMounts([{ mountPath: '/shared', name: 'shared' }])
       // Need to run as root because the GEM container does.