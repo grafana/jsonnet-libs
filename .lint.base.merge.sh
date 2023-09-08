b06bd41497f6bd043144f229df01e143615597fe
Julien Duchesne <julien.duchesne@grafana.com>
2023-09-08 10:15:31 -0400

    Write a base .lint file and apply it to all mixins. This will ignore new failing checks

---
+++ .lint.base.merge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Search for directories containing mixin.libsonnet files
+directories=$(find . -type f -name 'mixin.libsonnet' -exec dirname {} \;)
+
+# Iterate through the found directories
+for dir in $directories; do
+  lint_file="$dir/.lint"
+  lint_base_file=".lint.base.yaml"
+
+  # Check if a .lint file exists in the directory
+  if [ -f "$lint_file" ]; then
+    # Merge .lint and .lint.base.yaml using yq
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$lint_base_file" "$lint_file" > "$lint_file.tmp"
+    
+    # Replace the original .lint file with the merged content
+    mv "$lint_file.tmp" "$lint_file"
+    echo "Merged $lint_file with $lint_base_file"
+  else
+    # If .lint file doesn't exist, create it with the content of .lint.base.yaml
+    cp "$lint_base_file" "$lint_file"
+    echo "Created $lint_file from $lint_base_file"
+  fi
+done
