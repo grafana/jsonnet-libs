8768af8d6402784e71ee6f77631bc324ea627d48
Arik Grahl <github@arik-grahl.de>
2025-07-27 13:29:40 +0200

    chore: update Nix tooling for HAProxy

--- haproxy-mixin/nix/mixtool.nix
+++ haproxy-mixin/nix/mixtool.nix
@@ -1,22 +1,30 @@
-{ pkgs ? import <nixpkgs> }:
+{
+  pkgs ? import <nixpkgs>,
+}:
 
-with pkgs;
-buildGoModule rec {
+pkgs.buildGoModule (finalAttrs: {
   pname = "mixtool";
-  version = "bd0efc3";
+  version = "0-unstable-2025-07-07";
 
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "monitoring-mixins";
-    repo = pname;
-    rev = "${version}";
-    sha256 = "1kh2axna553q7lrmgak8l7jlnmbdfkfci240bqa3040pd82j3q1c";
+    repo = "mixtool";
+    rev = "1abe34c3187d53b795d0474535b476bc9b7500c3";
+    hash = "sha256-RRoz5Kp/IGkUD6XVK70+k4L05rYqhkqh6LpopihyEd8=";
   };
 
-  subPackages = [ "cmd/mixtool" ];
-  vendorSha256 = "10wvckrwrc7xs3dng9m6lznsaways2wycxnl9h8jynp4h2cw22ml";
+  vendorHash = "sha256-o9HNcq7XHXH/s6UthYADsktGh9NjgC1rVPbGP11Cfc0=";
 
-  meta = with lib; {
-    description = "Helper for easily working with Jsonnet mixins";
-    license = licenses.asl20;
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    description = "Helper for easily working with jsonnet mixins";
+    homepage = "https://github.com/monitoring-mixins/mixtool";
+    license = pkgs.lib.licenses.asl20;
+    maintainers = with pkgs.lib.maintainers; [ arikgrahl ];
+    mainProgram = "mixtool";
   };
-}
+})
